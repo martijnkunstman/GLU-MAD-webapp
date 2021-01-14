f110168c5f5a7a8d06b8be1fd8e9b435c955166a
martijnkunstman <martijn@kunstman.nl>
2021-01-14 16:28:20 +0100

    index.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
-var app = express();
-app.use(express.static('static'));
-app.listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+app.use(express.static('static'))
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."))
